f42532a1ead907eadf3f82647aed2244f27595a7
Leo <leolou.90@gmail.com>
2026-01-31 22:41:24 -0700

    create method to retreive passage according to mode and difficulty

--- src/services/passage-service/passage-service.ts
+++ src/services/passage-service/passage-service.ts
@@ -1,8 +1,29 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import {map, Observable } from 'rxjs'
+import { passageInterface } from '../../interfaces/interfaces';
 
 @Injectable({
   providedIn: 'root',
 })
 export class PassageService {
+
+  private url = 'assets/data/data.json';
+
+constructor(private http: HttpClient) {}  
   
+
+getFilteredPassages(mode: passageInterface['mode'], difficulty: passageInterface['difficulty']):Observable<passageInterface[]>{
+  return this.http.get<passageInterface[]>(this.url).pipe(
+      map(passages =>
+        passages.filter(p =>
+          p.mode === mode && p.difficulty === difficulty
+        )
+      )
+    );
+}
+
+test(){
+  return this.http.get(this.url);
+}
 }
