b8597ee7abcd5a07816cad88851e5884e208f46c
Leo <leolou.90@gmail.com>
2026-02-01 18:33:52 -0700

    add imports and create methods to set mode and difficulty

--- src/services/typing-state-service/typing-state-service.ts
+++ src/services/typing-state-service/typing-state-service.ts
@@ -1,8 +1,30 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject } from 'rxjs';
+import { PassageService } from '../passage-service/passage-service';
+
 
 @Injectable({
   providedIn: 'root',
 })
 export class TypingStateService {
   
+  private mode$ = new BehaviorSubject<'timed' | 'passage'>('timed');
+  private difficulty$ = new BehaviorSubject<'easy' | 'medium' | 'hard'>('easy');
+  private running$ = new BehaviorSubject<boolean>(false);
+
+  //Add services when finished
+  constructor(
+    private passageService: PassageService,
+  ) {}
+
+  
+  setMode(mode: 'timed' | 'passage') {
+    this.mode$.next(mode);
+  }
+
+  setDifficulty(level: 'easy' | 'medium' | 'hard') {
+    this.difficulty$.next(level);
+  }
+
+
 }
